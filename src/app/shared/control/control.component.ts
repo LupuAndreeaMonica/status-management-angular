2a689bfdb59dbc62ed3fd5018bf21190f269d74a
Andreea Lupu <andreea.lupu@scriptics.ro>
2025-05-12 09:51:25 +0300

    events listeners

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import {Component, input, ViewEncapsulation} from '@angular/core';
+import {Component, ElementRef, HostListener, inject, input, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-control', // host element
@@ -8,9 +8,19 @@ import {Component, input, ViewEncapsulation} from '@angular/core';
   styleUrl: './control.component.css',
   encapsulation: ViewEncapsulation.None,
   host: {
-    class: 'control'
+    class: 'control',
+    '(click)': 'onClick()'
   }
 })
 export class ControlComponent {
+  // @HostBinding('class') className = 'control';
+  // @HostListener('click') onClick() {
+  //   console.log('clicked')
+  // }
   label = input<string>()
+  private el = inject(ElementRef);
+  onClick() {
+    console.log('clicked')
+    console.log('el', this.el)
+  }
 }
